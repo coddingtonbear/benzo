570b052f1ed59dc68aaf371d0d692def9dd0035d
Adam Coddington <me@adamcoddington.net>
2016-03-21 23:12:15 -0700

    Handle empty documents for form submissions, too.

--- benzo/formatters/form.py
+++ benzo/formatters/form.py
@@ -7,6 +7,9 @@ class Formatter(Base):
     CONTENT_TYPE = 'application/x-www-form-urlencoded'
 
     def to_string(self, value):
+        if not value:
+            return ''
+
         return urllib.urlencode(value)
 
     def get_extension(self):
